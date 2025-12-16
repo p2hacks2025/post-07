4b48b7cc560eb41b8d0ab5c0bed9f5f03f767cfe
Akira-221inc <b1023077@fun.ac.jp>
2025-12-16 17:25:38 +0900

    いったん成功

--- front/my_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ front/my_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import flutter_ble_peripheral
+import flutter_blue_plus_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterBlePeripheralPlugin.register(with: registry.registrar(forPlugin: "FlutterBlePeripheralPlugin"))
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
 }
--- front/my_app/windows/flutter/generated_plugin_registrant.cc
+++ front/my_app/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
+#include <flutter_ble_peripheral/flutter_ble_peripheral_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  FlutterBlePeripheralPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterBlePeripheralPluginCApi"));
 }
