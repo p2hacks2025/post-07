264f6d23fad38fdd1cc6ee803db728ec781fbdbf
Akira-221inc <b1023077@fun.ac.jp>
2025-12-18 22:27:53 +0900

    おそらく適切なapiのURLを入れたら動くはず。

--- front/my_app/windows/flutter/generated_plugin_registrant.cc
+++ front/my_app/windows/flutter/generated_plugin_registrant.cc
@@ -8,6 +8,7 @@
 
 #include <file_selector_windows/file_selector_windows.h>
 #include <flutter_ble_peripheral/flutter_ble_peripheral_plugin_c_api.h>
+#include <flutter_inappwebview_windows/flutter_inappwebview_windows_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
@@ -15,6 +16,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
   FlutterBlePeripheralPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterBlePeripheralPluginCApi"));
+  FlutterInappwebviewWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterInappwebviewWindowsPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
